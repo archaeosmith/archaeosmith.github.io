f54fc649470977a358ac3e5c714952128177f6a5
Stefan L. Smith <110382694+archaeosmith@users.noreply.github.com>
2023-06-07 14:07:15 +0200

    Update chooser_test.js

--- chooser_test.js
+++ chooser_test.js
@@ -1,10 +1,11 @@
 options = {
         success: function(files){
-         
         },
         cancel: function(){
-                 
         },
+        linkType: "direct",
+        multiselect: true,
+        extensions:['.jpg'],
 };
 var button = Dropbox.createChooseButton(options);
 document.getElementById("dropboxContainer").appendChild(button);
