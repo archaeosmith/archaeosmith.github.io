0cd2076c0e42297932cdfde638cf6e0e7ec6a2b9
Stefan L. Smith <110382694+archaeosmith@users.noreply.github.com>
2023-06-10 00:47:48 +0200

    Update api_test.js

--- api_test.js
+++ api_test.js
@@ -1,7 +1,11 @@
-Dropbox.authorizedClient!.sharing.createSharedLink(path: "/test.txt").response({ response, error in
-    if let link = response {
-        print(link.url)
-    } else {
-        print(error!)
-    }
-})
+function httpPost(/2/files/get_temporary_link)
+{
+Host: https://api.dropboxapi.com
+User-Agent: api-explorer-client
+Authorization: Bearer sl.BgDRawsc66i2wtqi7A5Hdv-6xHSQXRyu_YNL0-1z5LQhQ5ak-M26tf07oxRA52DVJG3wRhOEqW2VpcIMln6cGAOMa-mkSTgdEehKl95fyvbJnchlTdcWWj6jkfu2MccArNXFGBY
+Content-Type: application/json
+
+{
+    "path": "/lizard.jpg"
+}
+}
